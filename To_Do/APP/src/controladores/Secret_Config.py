6eb8fb8e6f50b3c768037cd5d1a6c07de720c0e6
A.Guti <alejoguval08@gmail.com>
2024-09-15 12:50:58 -0500

    first version

--- To_Do/APP/src/controladores/Secret_Config.py
+++
@@ -1,7 +0,0 @@
-# Do not expose your Neon credentials to the browser
-
-PGHOST='ep-round-tree-a551uewg.us-east-2.aws.neon.tech'
-PGDATABASE='to_do'
-PGUSER='AGV'
-PGPASSWORD='TtfvlRibHh93'
-PGPORT=5432 
